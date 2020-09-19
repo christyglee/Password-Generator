164cb329671b5c0b30326419d61a3a86ce526921
Christy Lee <christy.g.lee@gmail.com>
2020-09-19 01:15:38 -0700

    console.log prompt user inputs

--- script.js
+++ script.js
@@ -8,15 +8,21 @@ var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "?
 // Prompt set-up
 function generatePassword() {
   var confirmLength = prompt("How many characters would you like your password to contain? (Must choose a number between 8 - 128.");{
+    console.log(confirmLength)
     if (confirmLength < 8 || confirmLength > 128) {
       alert("Password must be between 8 - 128");
       return false;
+
     }
     else if (confirmLength >= 8 || confirmLength <= 128) {
       var confirmLowercase =  confirm("Click OK if you would like lowercase letters.");
+      console.log(confirmLowercase);
       var confirmUppercase = confirm("Click OK if you would like uppercase letters");
+      console.log(confirmUppercase);
       var confirmNumber =  confirm("Click OK if you would like numeric charicters.");
+      console.log(confirmNumber);
       var confirmCharacter = confirm("Click OK if you would like special charicters.");
+      console.log(confirmCharacter)
     }
     else if (confirmLowercase != true && confirmUppercase != true && confirmNumber != true && confirmCharacter != true) {
       alert("You must select at least one character type.");
