722ee1f80b6a354f28717431632dbff8af556e8d
Christy Lee <christy.g.lee@gmail.com>
2020-09-19 00:57:06 -0700

    set up prompts and confirms with if/else statements

--- script.js
+++ script.js
@@ -5,12 +5,32 @@ var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "?", "<", ">", "[", "]", "{", "}"];
 
 
-
+// Prompt set-up
+function generatePassword() {
+  var confirmLength = prompt("How many characters would you like your password to contain? (Must choose a number between 8 - 128.");{
+    if (confirmLength < 8 || confirmLength > 128) {
+      alert("Password must be between 8 - 128");
+      return false;
+    }
+    else if (confirmLength >= 8 || confirmLength <= 128) {
+      var confirmLowercase =  confirm("Click OK if you would like lowercase letters.");
+      var confirmUppercase = confirm("Click OK if you would like uppercase letters");
+      var confirmNumber =  confirm("Click OK if you would like numeric charicters.");
+      var confirmCharacter = confirm("Click OK if you would like special charicters.");
+    }
+    else if (confirmLowercase != true && confirmUppercase != true && confirmNumber != true && confirmCharacter != true) {
+      alert("You must select at least one character type.");
+      return false;
+    }
+  }
+}
 
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+  
 
 // Write password to the #password input
 function writePassword() {
