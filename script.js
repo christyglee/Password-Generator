29d3c5699ee683a8d87b027998ddafa088ff44ad
Christy Lee <christy.g.lee@gmail.com>
2020-09-19 15:33:57 -0700

    updated typos

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ var characters = "!@#$%^&*()-+?<>[]{}";
 function generatePassword() {
 
   // Created values to clear old generated password
-  var userInput = "";
-  var newPassword = "";
+  var userInput = '';
+  var newPassword = '';
   var i = 0;
 
   // Set prompts and if statements
@@ -22,35 +22,43 @@ function generatePassword() {
       return;
     }
   // Set variable to confirm if user wants to include lowercase character
-    var confirmLowercase =  confirm("Click OK if you would like lowercase letters.");
+    var confirmLowercase = confirm("Click OK if you would like lowercase letters.");
     // if user confirms lowercase character, then set/add character type to password
     if (confirmLowercase) {
-      unserInput += lowercase;
+      userInput += lowercase;
       console.log(userInput);
     }
   // Set variable to confirm if user wants to unclude uppercase character
     var confirmUppercase = confirm("Click OK if you would like uppercase letters");
     // if user confirms uppercase character, then set/add character type to password
     if (confirmUppercase) {
-      unserInput += uppercase;
+      userInput += uppercase;
       console.log(userInput);
     }
   // Set variable to confirm if user wants to include number character
     var confirmNumber =  confirm("Click OK if you would like numeric charicters.");
     // if user confirms uppercase character, then set/add character type to password
     if (confirmNumber) {
+      userInput += number;
       console.log(userInput);
     }
   // Set variable to confirm if user wants to include number character
     var confirmCharacter = confirm("Click OK if you would like special charicters.");
     // if user confirms special character, then set/add character tyle to password 
     if (confirmCharacter) {
+      userInput += characters;
       console.log(userInput);
     }
     // Set alert if not at least one character is selected
     if (confirmLowercase != true && confirmUppercase != true && confirmNumber != true && confirmCharacter != true) {
       alert("You must select at least one character type.");
       return false;
+    }
+    // Set while loop to randomize password string value
+    while (i < confirmLength) {
+      newPassword += userInput[(Math.floor(Math.random() * userInput.length))];
+      console.log(newPassword);
+      i++
     }
     return newPassword;
 }
